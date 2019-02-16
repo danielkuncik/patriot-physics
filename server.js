7ee2344511b7f61d79cfbe37ab94afb078a2d74e
Daniel Kuncik <dkuncik@gmail.com>
2019-02-16 16:39:00 -0500

    build views folder; add basic header and footer partials

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const hbs = require('express-hbs');
 
 var app = express();
 
-app.engine('hbs', hbs.express4({
-    partialsDir: __dirname + '/views/partials'
-}));
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-
+app.engine('hbs', hbs.express4({
+   // extname: 'hbs',
+    defaultView: 'default.hbs',
+    partialsDir: __dirname + '/views/partials',
+    layoutsDir: __dirname + '/views/layouts'
+}));
 
 // ROUTES
 app.get('/',(req,res) => {
-    res.render('home.hbs');
+    res.render('home.hbs', {
+        layout:'default',
+        template:'home-template',
+        title: 'Home Page'
+    });
 });
 
 
