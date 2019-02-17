045ccfb8b01576babe0b45874376f7c22e581530
Daniel Kuncik <dkuncik@gmail.com>
2019-02-17 10:46:07 -0500

    transfer computational physsics html files from older project; design routing system to display said computational physics html files

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var app = express();
 
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', [__dirname + '/views',__dirname + '/content']);
 app.engine('hbs', hbs.express4({
    // extname: 'hbs',
     defaultView: 'default.hbs',
     partialsDir: __dirname + '/views/partials',
     layoutsDir: __dirname + '/views/layouts'
 }));
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/content'));
 
 // ROUTES
 app.get('/',(req,res) => {
@@ -22,50 +24,63 @@ app.get('/',(req,res) => {
     });
 });
 app.get('/about',(req,res) => {
-    res.render('about.hbs', {
+    res.render('aboutEntryPage.hbs', {
         layout:'default',
        // template:'about-template',
         title:'About'
     })
 });
 app.get('/calendars', (req, res) => {
-    res.render('calendars.hbs', {
+    res.render('calendarsEntryPage.hbs', {
         layout:'default',
         title:'Calendars'
     })
 });
 app.get('/units', (req, res) => {
-    res.render('units.hbs', {
+    res.render('unitsEntryPage.hbs', {
         layout:'default',
         title:'Units'
     })
 });
 app.get('/labs', (req, res) => {
-    res.render('labs.hbs', {
+    res.render('labsEntryPage.hbs', {
         layout:'default',
         title:'Labs'
     })
 });
 app.get('/writing', (req, res) => {
-    res.render('writing.hbs', {
+    res.render('writingEntryPage.hbs', {
         layout:'default',
         title:'Writing'
     })
 });
-app.get('/computation', (req, res) => {
-    res.render('computation.hbs', {
+app.get('/computationEntry', (req, res) => {
+    res.render('computationEntryPage.hbs', {
         layout:'default',
         title:'Computational Physics'
     })
 });
+app.get('/computation/:type/:name', (req, res) => {
+    if (req.params.type === 'entry') {
+        res.render('computationEntryPage.hbs', {
+            layout:'default',
+            title:'Computational Physics'
+        })
+    } else {
+        res.render(`computation/${req.params.type}/${req.params.name}.hbs`, {
+            layout:'computationLayout.hbs',
+            id: req.params.uid
+        })
+    }
+});
 app.get('/enrichment', (req, res) => {
-    res.render('enrichment.hbs', {
+    res.render('enrichmentEntryPage.hbs', {
         layout:'default',
         title:'Enrichment'
     })
 });
 app.get('/jokes', (req, res) => {
-    res.render('jokes.hbs', {
+    res.render('jokesEntryPage.hbs', {
         layout:'default',
         title:'Jokes'
     })
