058a25bfd4b349b88027d328db283e380f0e1981
Daniel Kuncik <dkuncik@gmail.com>
2019-02-17 17:10:20 -0500

    add seed of json list of units; write helper to present this as html list

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-hbs');
+var unitMap = require(__dirname + '/content/units/unit_map');
 
 var app = express();
 
@@ -15,6 +16,29 @@ app.engine('hbs', hbs.express4({
 app.use(express.static(__dirname + '/public'));
 //app.use(express.static(__dirname + '/content'));
 
+
+hbs.registerHelper('listUnits', () => {
+    var unitList = "<ul>";
+    Object.values(unitMap).forEach((unitCluster) => {
+        unitList = unitList + `<li>${unitCluster.name}`;
+        unitList = unitList + "<ul>";
+        Object.values(unitCluster.units).forEach((unit) => {
+            unitList = unitList + `<li>${unit.name}`;
+            unitList = unitList + "<ul>";
+            Object.values(unit.pods).forEach((pod) => {
+                unitList = unitList + `<li>${pod.name}</li>`
+            });
+            unitList = unitList + "</ul>";
+            unitList = unitList + "</li>";
+        });
+        unitList = unitList + "</ul>";
+        unitList = unitList + "</li>";
+    });
+    unitList = unitList + "</ul>";
+    return new hbs.SafeString(unitList);
+});
+
+
 // ROUTES
 app.get('/',(req,res) => {
     res.render('home.hbs', {
@@ -28,62 +52,63 @@ app.get('/about',(req,res) => {
         layout:'default',
        // template:'about-template',
         title:'About'
-    })
+    });
 });
 app.get('/calendars', (req, res) => {
     res.render('calendarsEntryPage.hbs', {
         layout:'default',
         title:'Calendars'
-    })
+    });
 });
 app.get('/units', (req, res) => {
     res.render('unitsEntryPage.hbs', {
         layout:'default',
-        title:'Units'
-    })
+        title:'Units',
+        unitMap:unitMap
+    });
 });
 app.get('/labs', (req, res) => {
     res.render('labsEntryPage.hbs', {
         layout:'default',
         title:'Labs'
-    })
+    });
 });
 app.get('/writing', (req, res) => {
     res.render('writingEntryPage.hbs', {
         layout:'default',
         title:'Writing'
-    })
+    });
 });
 app.get('/computationEntry', (req, res) => {
     res.render('computationEntryPage.hbs', {
         layout:'default',
         title:'Computational Physics'
-    })
+    });
 });
 app.get('/computation/:type/:name', (req, res) => {
     if (req.params.type === 'entry') {
         res.render('computationEntryPage.hbs', {
             layout:'default',
             title:'Computational Physics'
-        })
+        });
     } else {
         res.render(`computation/${req.params.type}/${req.params.name}.hbs`, {
             layout:'computationLayout.hbs',
             id: req.params.uid
-        })
+        });
     }
 });
 app.get('/enrichment', (req, res) => {
     res.render('enrichmentEntryPage.hbs', {
         layout:'default',
         title:'Enrichment'
-    })
+    });
 });
 app.get('/jokes', (req, res) => {
     res.render('jokesEntryPage.hbs', {
         layout:'default',
         title:'Jokes'
-    })
+    });
 });
 // sending a joke image image
 app.get('/getJoke/:imageName', (req,res)=>{
