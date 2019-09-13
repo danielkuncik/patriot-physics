80ec2b82789ca45daebb653ca69272c1a4bd310e
Daniel Kuncik <dkuncik@gmail.com>
2019-09-13 14:41:20 -0400

    add hw for 9-16-19

--- public/javascript/makeSpecificProblems.js
+++ public/javascript/makeSpecificProblems.js
@@ -1,10 +1,13 @@
 
-function makeCircuitProblem(type, voltage, resistorArray, powerRowBoolean) {
+function makeCircuitProblem(type, voltage, resistorArray, powerRowBoolean, note) {
     let circuit;
     if (type === 'series') {
         circuit = makeSeriesCircuit(voltage, resistorArray);
     } else if (type === 'parallel') {
         circuit = makeParallelCircuit(voltage, resistorArray);
+    }
+    if (powerRowBoolean === undefined) { // default is to ahve a power row
+        powerRowBoolean = true;
     }
     let myTable = new CircuitTable(resistorArray.length, powerRowBoolean);
     let containerDiv = $("<div class = 'container'></div>");
@@ -13,6 +16,9 @@ function makeCircuitProblem(type, voltage, resistorArray, powerRowBoolean) {
     let tableDiv = $("<div class = 'col-md-6'></div>");
     $(tableDiv).append(myTable.table);
     $(circuitDiv).append(circuit.drawCanvas(400,300));
+    if (note) {
+        $(circuitDiv.append($(`<p>${note}</p>`)));
+    }
     $(rowDiv).append(circuitDiv);
     $(rowDiv).append(tableDiv);
     $(containerDiv).append(rowDiv);
