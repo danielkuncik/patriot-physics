6a17fecf9e9e3bafcec2d4f31f535381791b3989
Daniel Kuncik <dkuncik@gmail.com>
2019-11-06 14:00:38 -0500

    add homework

---
+++ public/javascript/physicsFunctions.js
@@ -0,0 +1,10 @@
+
+
+// for an object that experiences
+// a particular maximum static friction and particular magnitude of kinetic friction
+// draws a graph of force-time of this particular function
+// assuming you push until you exceed max static friction
+// then push at a constant velocity thereafter
+function pushingWithFrictionFunction(maxStaticFriction, kineticFriction) {
+    // validation: max static friciton must be greater than kinetic friction
+}
