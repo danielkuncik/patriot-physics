cef8b4164012410f5f97ed8e1bdd5b5f326073c2
danielkuncik <danielkuncik@admins-MacBook-Air.local>
2019-11-14 13:30:15 -0500

    change passcode

--- server.js
+++ server.js
@@ -559,7 +559,7 @@ app.get('/quizzes/:unitClusterKey/:unitKey', (req, res) => {
 
 /// add security for quizzes!
 
-const quizPasscode = '529525';
+const quizPasscode = '639201';
 
 // individual quiz page
 app.get('/quizzes/:unitClusterKey/:unitKey/:podKey', (req, res) => {
