18a71e3d9a2f530997fbfa508b7bf0cce564552f
Daniel Kuncik <dkuncik@gmail.com>
2019-12-03 20:39:22 -0500

    delete unecessary pdfs

--- server.js
+++ server.js
@@ -576,7 +576,7 @@ app.get('/quizzes/:unitClusterKey/:unitKey', (req, res) => {
 
 /// add security for quizzes!
 
-const quizPasscode = '329821';
+const quizPasscode = '532971';
 
 
 // individual quiz page
