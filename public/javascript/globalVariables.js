2d5eee7a039d3e89ed8758f9f62ac6bfd10f2dbd
Daniel Kuncik <dkuncik@gmail.com>
2019-12-23 14:47:35 -0500

    add global variables javascript file

---
+++ public/javascript/globalVariables.js
@@ -0,0 +1,9 @@
+let constantVelocityEquation = '\\( \\Delta x = v \\cdot \\Delta t \\)';
+
+let kinematicEquation1 = '\\( v_f = v_i + a \\cdot \\Delta t \\)';
+
+let kinematicEquation2 = '\\( \\Delta x = v_i \\cdot \\Delta t + \\frac{1}{2} a \\left( \\Delta t \\right)^2 \\)';
+
+let kinematicEquation3 = '\\( \\Delta x = \\left( \\frac{v_f + v_i}{2} \\right) \\Delta t \\)';
+
+let kinematicEquation4 = '\\( v_f^2 =v_i^2 + 2 a \\cdot \\Delta x \\)';
