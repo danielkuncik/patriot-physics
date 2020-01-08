0372e82b9788501045876e7a356d48b905eecd89
Daniel Kuncik <dkuncik@gmail.com>
2020-01-08 11:49:44 -0500

    add new version of newtons second law quiz

--- server.js
+++ server.js
@@ -576,7 +576,7 @@ app.get('/quizzes/:unitClusterKey/:unitKey', (req, res) => {
 
 /// add security for quizzes!
 
-const quizPasscode = '836521';
+const quizPasscode = '538174';
 
 
 // individual quiz page
