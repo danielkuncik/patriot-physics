50f577d72d4b67e32e9849b1a016fe798906681e
Daniel Kuncik <dkuncik@gmail.com>
2020-01-18 11:51:48 -0500

    change quiz passcode

--- server.js
+++ server.js
@@ -576,7 +576,7 @@ app.get('/quizzes/:unitClusterKey/:unitKey', (req, res) => {
 
 /// add security for quizzes!
 
-const quizPasscode = '243894';
+const quizPasscode = '367802';
 
 
 // individual quiz page
