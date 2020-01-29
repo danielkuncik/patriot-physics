e4267c9044f9ac833c713c17196713ee3d06fac0
Daniel Kuncik <dkuncik@gmail.com>
2020-01-29 13:06:36 -0500

    add hw for ap

--- server.js
+++ server.js
@@ -576,7 +576,7 @@ app.get('/quizzes/:unitClusterKey/:unitKey', (req, res) => {
 
 /// add security for quizzes!
 
-const quizPasscode = '523890';
+const quizPasscode = '203857';
 
 
 // individual quiz page
