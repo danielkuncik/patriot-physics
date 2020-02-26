a9b171d56905236b0fa0be4e7da7039d3a9a7b52
Daniel Kuncik <dkuncik@gmail.com>
2020-02-26 10:40:59 -0500

    change quiz passcode

--- display.js
+++ display.js
@@ -566,7 +566,7 @@ display_quiz_unit_page = (req, res) => {
     })
 };
 
-const quizPassword = 'ibis';
+const quizPassword = 'hippopotamus';
 
 function quizAccess(section, level, enteredPassword) {
     let result = false;
