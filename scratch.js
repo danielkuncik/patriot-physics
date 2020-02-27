b79c49229e2f541f9769fb4eb16f1b682fe34202
Daniel Kuncik <dkuncik@gmail.com>
2020-02-27 14:00:12 -0500

    oragnize home page into three sections

--- scratch.js
+++ scratch.js
@@ -2,10 +2,39 @@ const unitMap2 = require(__dirname + '/public/unit_map');
 
 const alphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+const alphabetDictionary = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26
+};
 
 class GradeMap {
     constructor() {
         this.map = {};
+        this.quizCodes = {};
         this.overallLevel = 0;
         Object.entries(unitMap2).forEach((entry_A) => {
             let superUnitKey = entry_A[0];
@@ -34,6 +63,7 @@ class GradeMap {
                                 grade: 0,
                                 pending: false
                             };
+                            let podCode =
                             podsPerLevel[pod.level] += 1;
                         });
 
