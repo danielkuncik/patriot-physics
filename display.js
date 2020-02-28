735fff02711f7df4fe9f4bc2449f3d33a03668e2
Daniel Kuncik <dkuncik@gmail.com>
2020-02-28 13:23:10 -0500

    change passcode and fix typos

--- display.js
+++ display.js
@@ -589,7 +589,7 @@ display_quiz_unit_page = (req, res) => {
     })
 };
 
-const quizPassword = 'alligator';
+const quizPassword = 'gfdiiuvnaiu';
 
 function quizAccess(section, level, enteredPassword) {
     let result = false;
