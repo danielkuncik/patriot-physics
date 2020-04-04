b65c45bd2d26903e46f6ae70ef9f460ae00e522e
Daniel Kuncik <dkuncik@gmail.com>
2020-04-04 14:00:47 -0400

    make diagram object for circular motion and begin work on circular motion basics pod

---
+++ public/javascript/diagrams/circularMotionObject.js
@@ -0,0 +1,100 @@
+
+
+class CircularMotionDiagram extends Diagram {
+    constructor(direction, radius, numberOfDirectionIndicators, firstDirectionIndicator) {
+        if (direction === undefined) {
+            direction = 'counterclockwise'
+        }
+        if (direction !== 'counterclockwise' && direction !== 'clockwise') {
+            console.log('ERROR: direction of circular motion must be either clockwise or counterclockwise. Setting to counterclockwise');
+            direction = 'counterclockwise';
+        }
+        if (numberOfDirectionIndicators === undefined) {
+            numberOfDirectionIndicators = 4;
+        }
+        if (firstDirectionIndicator === undefined) {
+            firstDirectionIndicator = 0;
+        }
+        if (radius === undefined) {radius = 1;}
+
+        super();
+
+        this.direction = direction;
+        this.radius = radius;
+
+        super.addCircle(origin, this.radius);
+
+        let k;
+        for (k = 0; k < numberOfDirectionIndicators; k++) {
+            this.addArrowheadOnCircle(firstDirectionIndicator + 2 * Math.PI / numberOfDirectionIndicators * k);
+        }
+
+    }
+
+    // gets the angle that a circle is pointing
+    getPointingAngle(positionInRadians) {
+        let pointingAngle = positionInRadians;
+        if (this.direction === 'clockwise') {
+            pointingAngle += Math.PI / 2;
+        } else if (this.direction === "counterclockwise") {
+            pointingAngle -= Math.PI / 2;
+        }
+        return pointingAngle
+    }
+
+    // add arrowhead that shows the direction something is moving in the circle
+    addArrowheadOnCircle(positionInRadians, arrowheadLength, arrowheadAngleInDegrees) {
+        if (arrowheadLength === undefined) {
+            arrowheadLength = this.radius / 5;
+        }
+        if (arrowheadAngleInDegrees === undefined) {
+            arrowheadAngleInDegrees = 35;
+        }
+        const arrowheadAngleInRadians = convertDegreesToRadians(arrowheadAngleInDegrees);
+        const pointingAngle = this.getPointingAngle(positionInRadians);
+        let centerPoint = this.getPointOnCircle(positionInRadians);
+        let pointA = centerPoint.translateAndReproducePolar(arrowheadLength, pointingAngle - Math.PI + arrowheadAngleInRadians);
+        let pointB = centerPoint.translateAndReproducePolar(arrowheadLength, pointingAngle - Math.PI - arrowheadAngleInRadians);
+
+        super.addSegment(centerPoint, pointA);
+        super.addSegment(centerPoint, pointB);
+    }
+
+    getPointOnCircle(positionInRadians) {
+        return new Point(this.radius * Math.cos(positionInRadians), this.radius * Math.sin(positionInRadians))
+    }
+
+    addVisiblePoint(positionInRadians) {
+        super.addBlackCircle(this.getPointOnCircle(positionInRadians), this.radius / 10);
+    }
+
+    addVelocityArrow(positionInRadians, length, label) {
+        if (length === undefined) {
+            length = this.radius * 1.2;
+        }
+        const pointingAngle = this.getPointingAngle(positionInRadians);
+        let pointA = this.getPointOnCircle(positionInRadians);
+        let pointB = pointA.translateAndReproducePolar(length, pointingAngle);
+
+        super.addArrow(pointA, pointB);
+        if (label) {
+            super.labelLineOutside(pointA, pointB, label, undefined, undefined, this.direction);
+        }
+
+    }
+
+    addCentripetalForce(positionInRadians, length, label) {
+        if (length === undefined) {
+            length = this.radius * 0.75;
+        }
+        const inwardAngle = positionInRadians + Math.PI;
+        let pointA = this.getPointOnCircle(positionInRadians);
+        let pointB = pointA.translateAndReproducePolar(length, inwardAngle);
+
+        super.addArrow(pointA, pointB);
+        if (label) {
+            super.labelLineAbove(pointA, pointB, label);
+        }
+    }
+
+}
