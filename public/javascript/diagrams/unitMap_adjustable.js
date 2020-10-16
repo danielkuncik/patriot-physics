41fb260ddee03d6f5dbfe51d16ea8d8d30a6619e
Daniel Kuncik <dkuncik@gmail.com>
2020-10-15 22:36:21 -0400

    slight adjustmenst to quizzes and adding answers

---
+++ public/javascript/diagrams/unitMap_adjustable.js
@@ -0,0 +1,17 @@
+/*
+This is a unit map that students can adjust in order to determine what their grade WOULD be if they reached
+a particular point in the unit map
+ */
+
+// this needs to incorporate the back end????
+// this will take some work, but it will be fun???
+
+
+// its the only way to stop the endless questions about 'what will my grade be if.....'
+
+// it really should be an html file???
+
+// create a helper and some back end programs that get the 'unitObjectWithGrades' to the front end
+function drawAdjustableUnitMap(unitObjectWithGrades, goalLevel, officialUnitName) {
+
+}
