305f7ba583ab52834737854b7ea313b668ffab72
Daniel Kuncik <dkuncik@gmail.com>
2020-12-17 19:04:18 -0500

    fix bug in practice overdue calculation

--- server.js
+++ server.js
@@ -314,7 +314,7 @@ const checkQuizAccess2 = (req, res, next) => {
                         req.practiceObject["practicePending"] = req.gradeMap[req.superUnitKey].units[req.unitKey].pods[req.podKey].practicePending;
                         req.practiceObject["currentTopScore"] = req.gradeMap[req.superUnitKey].units[req.unitKey].pods[req.podKey].practiceScore;
                         req.practiceObject["comment"] = req.gradeMap[req.superUnitKey].units[req.unitKey].pods[req.podKey].practiceComment;
-                        let practiceDueDate = requirements.practiceDueDate ? requirements.practiceDueDate : thisDueDate;
+                        let practiceDueDate = requirements.practiceDueDate ? requirements.practiceDueDate : quizDueDate;
                         let practiceDueDateObject = new Date(practiceDueDate);
                         req.practiceObject.overdue = practiceDueDate - now < 0;
                         req.practiceObject.dueDate = practiceDueDate;
