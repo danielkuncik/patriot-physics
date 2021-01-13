4cb0f7998c6778b4bc25d2e481e009613609af11
Daniel Kuncik <dkuncik@gmail.com>
2021-01-13 14:26:34 -0500

    add more to scale calculation

--- scaleCalculation.py
+++ scaleCalculation.py
@@ -6,23 +6,22 @@ print ('hello world')
 
 ## next step => mess with the base! then see if that makes it come out to rounder numbers
 ## then => adjust a few numbers +1 or -1 to make them come out rounder
-base = 100
+base = 143
 r = 1.072
 sum = 0
 
-step = base
+step = base / r ## to offset multiplying for step 0
 
 ten_steps = 0
 
 print('Score','Step','Sum')
 print(50,'N/A',0)
-for i in range(1,52):
+for i in range(1,11):
 	step *= r
 	step_rounded = round(step)
 	sum += step_rounded
-	ten_steps += step_rounded
-	if ((i - 1) % 10 == 0 and i != 1): ## there is an error here, make sure it is calculating this correctly!
-		print('               ' + str(i - 11 + 50) + ' to ' + str(i-1 + 50), int(ten_steps - step_rounded))
-		ten_steps = 0
 	print(i + 50, '+' + str(int(step_rounded)),int(sum))
+
+
+
 
---
+++ scaleCalculation2.py
@@ -0,0 +1,18 @@
+
+
+theScale = [143,154,165,177,189,203,218,234,249,268]
+
+multiplier = 1.5
+
+def nextStep(i):
+    return theScale[(i - 50) % 10 - 1] * ((i - 51) / 10 + 1) * multiplier
+
+sum = 0
+for i in range(51,101):
+    step = nextStep(i)
+    sum += step
+    print(i, step, sum)
+
+    # is this what i wanted?
+
+    # 50 = 0; 60 = 2000; 70 = 6000; 80 = 12000; 90 = 20000; 100 = 30000
