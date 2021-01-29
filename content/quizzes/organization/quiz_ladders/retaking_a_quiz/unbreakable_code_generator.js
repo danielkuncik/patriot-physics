8012c1f75c3aeef08e6f9efd277adc1ebff49826
Daniel Kuncik <dkuncik@gmail.com>
2021-01-29 14:13:29 -0500

    new version of quiz; start on unbreakable code quiz

--- content/quizzes/organization/quiz_ladders/retaking_a_quiz/unbreakable_code_generator.js
+++ content/quizzes/organization/quiz_ladders/retaking_a_quiz/unbreakable_code_generator.js
@@ -1,5 +1,79 @@
-const all_ascii_characters =
+const available_characters =
     ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+     '1','2','3','4','5','6','7','8','9','0',
+     '`','~','!','@','#','$','%','^','&','*','(',')','-','_','+','=',
+     '[',']','{','}','\\','|',';',':','\'','"',',','<','.','>','/','?'
+    ];
 
-    ]
+console.log(available_characters.length); /// 94
+
+
+const state_capitols = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rogue",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismark",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne"
+};
+
+const elements = [
+    "Hydrogen",
+    "Helium",
+    "Lithium",
+    "Beryllium",
+    ""
+];
+
+// 13 is the longest
+
+// so, any state capitol question must be 22 letters to be unsolvable
+// this means that three of them will be 66
+//capitolofmassachusetts
+
+// if i add "what is the" => it goes to 31 letters
